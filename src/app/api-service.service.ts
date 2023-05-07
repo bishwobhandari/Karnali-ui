f5f96be390b0459ee2ea3f8209c9fcb2063d8907
Bishwo Bhandari <bhanbish@isu.edu>
2023-05-07 11:03:44 -0400

    new changes

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -23,8 +23,44 @@ export class ApiServiceService {
     return this.http.get<any>(url, httpOptions);
   }
 
+  getDistricts(): Observable<any> {
+    const url = this.apiUrl+'api/districts/all';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http.get<any>(url, httpOptions);
+  }
+
+  getConstituencies(): Observable<any> {
+    const url = this.apiUrl+'api/constituency/all';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http.get<any>(url, httpOptions);
+  }
+
   getDistrictsByProvince(currentProvince:number): Observable<any> {
     const url = this.apiUrl+'api/districts/getDistrictsByProvince/'+currentProvince;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http.get<any>(url, httpOptions);
+  }
+
+  getConstituenciesByDistrict(currentDistrict:any): Observable<any> {
+
+    const url = this.apiUrl+'api/constituency/getConstituenciesByDistrict/'+currentDistrict;
+
+    console.log(url)
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
--- src/app/candidates/candidates.component.ts
+++ src/app/candidates/candidates.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiServiceService } from '../api-service.service';
+import { forkJoin, map } from 'rxjs';
 
 @Component({
   selector: 'app-candidates',
@@ -10,39 +11,62 @@ export class CandidatesComponent {
 
   provinces: any[] = [];
   districts:any[]=[];
+  constituencies:any[] = [];
   currentProvince=1;
+  currentDistrict=1;
+  currentConstituency=1;
+  dataLoaded=false;
 
   constructor(private apiService: ApiServiceService) { }
 
+ 
+
   ngOnInit(): void {
-    this.apiService.getProvinces().subscribe(
-      data => {
+   
+      forkJoin({
+        provinces: this.apiService.getProvinces(),
+        districts: this.apiService.getDistricts(),
+        constituencies: this.apiService.getConstituencies()
+      })
+      .pipe(
+        map(response => {
+          const provinces = <Array<any>>response.provinces;
+          const districts = <Array<any>>response.districts;
+          const constituencies = <Array<any>>response.constituencies;
+          const result: any[] = [];
+          provinces.map((province: any) => {
+            result.push({
+              ...province, 
+              ...districts.find((district: any) => district.province_id === province.id), 
+              ...constituencies.find((constituency: any) => constituency.district_id === constituency.userId)})
+          });
+  
+          console.log("result ", result)
+          return result;
+        })
+      )
+      .subscribe((data) => {
+        console.log(data)
         this.provinces = data;
-        console.log(this.provinces)
-      },
-      error => {
-        console.error(error);
-      }
-    );
-
-    this.apiService.getDistrictsByProvince(this.currentProvince).subscribe(
-      data => {
-        this.districts = data;
-        console.log(this.districts)
-      },
-      error => {
-        console.error(error);
-      }
-    );
-  }
-
-  onOptionSelected() {
-    // Do something when the selected option is changed
+      });
     
-    this.apiService.getDistrictsByProvince(this.currentProvince).subscribe(
+  }
+
+  updateCurrentDistrict(){
+    this.currentDistrict=this.districts[0];
+  }
+
+  async loadData(){
+ 
+
+
+  }
+
+ async getProvinces(){
+     await this.apiService.getProvinces().subscribe(
       data => {
-        this.districts = data;
-        console.log(this.districts)
+        console.log("get provinces called", data)
+        this.provinces = data;
       },
       error => {
         console.error(error);
@@ -50,4 +74,43 @@ export class CandidatesComponent {
     );
   }
 
+ async  getDistricts(){
+  await this.apiService.getDistricts().subscribe(
+    data => {
+      console.log("get districts called", data)
+      this.districts = data;
+    },
+    error => {
+      console.error(error);
+    }
+  );
+  }
+
+  async  getConstituencies(){
+    await this.apiService.getConstituencies().subscribe(
+      data => {
+        console.log("get constituency called", data)
+        this.constituencies=data;
+   
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  
+  }
+
+  setCurrentDistrict(){
+    this.currentDistrict=this.districts[0].id;
+    console.log("current district is" ,this.currentDistrict)
+  }
+
+  setCurrentConstituency(){
+    console.log(this.constituencies)
+    if(this.constituencies.length > 0){
+      this.currentConstituency=this.constituencies[0].id;
+    }
+
+    console.log("current district is" ,this.currentConstituency)
+  }
 }
